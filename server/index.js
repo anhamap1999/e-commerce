ba35772808c3362e39514f38e4c81ee742c01db8
A N H <51475160+anhamap1999@users.noreply.github.com>
2020-10-24 09:32:16 +0700

    connect db

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+const { PORT, DB_CONNECT } = process.env;
+mongoose
+  .connect(DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(console.log("CONNECTED DATABASE"));
 
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log("LISTEN TO PORT ", 5000);
+app.listen(PORT || 5000, () => {
+  console.log("LISTEN TO PORT ", PORT);
 });
